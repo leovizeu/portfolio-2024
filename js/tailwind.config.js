02a9e1262d826492d5d60f1cb5a9f1957e8aef67
Vizeu <leovizeu@hotmail.com>
2023-12-21 20:10:17 -0300

    added tailwindCSS and this commit is before the Form change from Materialize to CSS, if it doesn't work i'll reset the staged commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [/D:Vizeu/Code/HMTL/portfolio-2024/index.html, /D:Vizeu/Code/HTML/portfolio-2024/js/script.js],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
