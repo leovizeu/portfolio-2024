84e6ee1b414980a80d57f7ccd39546fe48a64dc8
Vizeu <leovizeu@hotmail.com>
2023-12-21 22:02:30 -0300

    did reload the last commit, i'll stick with materialize for now

--- js/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [/D:Vizeu/Code/HMTL/portfolio-2024/index.html, /D:Vizeu/Code/HTML/portfolio-2024/js/script.js],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
